c4fdb2555451c2056b567009f836f8f391df938a
Hai Pham <swiften.svc@gmail.com>
2018-11-09 21:30:34 +0800

    feat(apis): add api method to save vocabularies

--- src/apis/vocab.ts
+++ src/apis/vocab.ts
@@ -1,4 +1,5 @@
 import { ApisauceInstance } from 'apisauce';
+import * as Bluebird from 'bluebird';
 import { Vocab } from 'data';
 import { Never } from 'javascriptutilities';
 
@@ -6,6 +7,12 @@ export default function(api: ApisauceInstance) {
   return {
     async getVocabularies() {
       return (await api.get<Never<Array<Never<Vocab>>>>('/vocabularies')).data;
+    },
+
+    async saveVocabularies(vocabs: Array<Partial<Vocab>>) {
+      return Bluebird.map(vocabs, vocab =>
+        api.patch<Partial<Vocab>>('/vocabularies', vocab)
+      ).map(response => response.data);
     }
   };
 }
