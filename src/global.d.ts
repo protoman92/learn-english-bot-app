797c9d5bd4bc32ff7e7d19cdb774c1895ceb71b1
Hai Pham <swiften.svc@gmail.com>
2018-11-09 16:56:58 +0800

    feat(app): set up sagas in main script

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  // tslint:disable-next-line
+  interface ProcessEnv {
+    readonly BASE_API_URL: string;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
+import createAPI from 'api';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import createSagas from 'sagas';
 import App from './component/app/App';
 import './index.css';
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer);
+const api = createAPI({ baseURL: process.env.REACT_APP_BASE_API_URL });
+const sagas = createSagas(api);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(sagas);
 
 ReactDOM.render(
   <Provider store={store}>
