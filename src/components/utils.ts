82798a4d321da689bc2de911fcdb4c9e8e51563c
Hai Pham <swiften.svc@gmail.com>
2018-11-10 02:54:11 +0800

    feat(components): add onlyUpdateWhenDeepEqual hoc

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,3 +1,24 @@
+import { shouldUpdate } from 'recompose';
+
+// tslint:disable-next-line
+const deepEqual = require('deep-equal');
+
 export namespace TextFieldFont {
   export const body1 = '0.875rem';
+}
+
+export function onlyUpdateWhenDeepEqual<P>() {
+  return shouldUpdate<P>((p1, p2) => {
+    for (const key of Object.keys(p1)) {
+      if (p1[key] instanceof Function || p2[key] instanceof Function) {
+        continue;
+      }
+
+      if (!deepEqual(p1[key], p2[key])) {
+        return true;
+      }
+    }
+
+    return false;
+  });
 }
