5eeba4a4a69e3617750df9f1cad7b06b41ea9a37
Hai Pham <swiften.svc@gmail.com>
2018-11-11 01:21:49 +0800

    feat(sagas): add put action for progress in vocab sagas

--- src/sagas/vocabulary.ts
+++ src/sagas/vocabulary.ts
@@ -1,26 +1,37 @@
 import { ActionKey, getters, setters } from 'actions/vocabulary';
-import { Vocabulary } from 'data';
-import { Never, Try } from 'javascriptutilities';
+import { Unpacked } from 'javascriptutilities';
 import { all, call, put, select, takeLatest } from 'redux-saga/effects';
 
 export default function(vocabApi: import('apis').Api['vocabulary']) {
   function* fetchVocabularies(api: typeof vocabApi) {
-    const vocabs = yield call(api.fetchVocabularies);
+    yield put(setters.setProgress(true));
+
+    const vocabs: Unpacked<
+      ReturnType<typeof api['fetchVocabularies']>
+    > = yield call(api.fetchVocabularies);
+
     yield put(setters.setVocabularies(vocabs));
+    yield put(setters.setProgress(false));
   }
 
   function* saveVocabularies(api: typeof vocabApi) {
-    const vocabs: Try<Array<Never<Partial<Vocabulary>>>> = yield select(
-      getters.getAllVocabularies
-    );
+    yield put(setters.setProgress(true));
+
+    const vocabs: ReturnType<
+      typeof getters['getAllVocabularies']
+    > = yield select(getters.getAllVocabularies);
 
     const validVocabs = vocabs
       .getOrElse([])
       .filter(vocab => !!vocab)
       .map(vocab => vocab!);
 
-    const updatedVocabs = yield call(api.saveVocabularies, validVocabs);
+    const updatedVocabs: Unpacked<
+      ReturnType<typeof api['saveVocabularies']>
+    > = yield call(api.saveVocabularies, validVocabs);
+
     yield put(setters.setVocabularies(updatedVocabs));
+    yield put(setters.setProgress(false));
   }
 
   return function*() {
