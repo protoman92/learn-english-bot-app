8866fd9fa49f35e76a5704bd144735241011b8f4
Hai Pham <swiften.svc@gmail.com>
2018-11-10 11:52:26 +0800

    feat(vocab-meaning): allow PoS selection in vocab meaning item

--- src/components/vocab-meaning/item/component.tsx
+++ src/components/vocab-meaning/item/component.tsx
@@ -1,7 +1,9 @@
-import { TextField } from '@material-ui/core';
+import { MenuItem, TextField } from '@material-ui/core';
 import { TextFieldProps } from '@material-ui/core/TextField';
 import { getters, setters } from 'actions/vocab-meaning';
 import { TextFieldFont } from 'components/utils';
+import { Selectable } from 'data';
+import { selectablePoS as allPoS } from 'data/pos';
 import { UndefinedProp } from 'javascriptutilities';
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -15,12 +17,25 @@ type Props = Readonly<{
   isDummy: boolean;
 }>;
 
-type DispatchProps = Readonly<{ changeDef: TextFieldProps['onChange'] }>;
-type StateProps = UndefinedProp<Readonly<{ def: string }>>;
+type DispatchProps = Readonly<{
+  changeDef: TextFieldProps['onChange'];
+  changePos: TextFieldProps['onChange'];
+}>;
+
+type StateProps = UndefinedProp<
+  Readonly<{
+    def: string;
+    pos: string;
+    selectablePoS: Array<Selectable<string>>;
+  }>
+>;
 
 function VocabMeaningItem({
   changeDef,
-  def = ''
+  changePos,
+  def = '',
+  pos = '',
+  selectablePoS = []
 }: Props & DispatchProps & StateProps) {
   return (
     <div className="vocab-meaning-item-container">
@@ -33,7 +48,21 @@ function VocabMeaningItem({
         value={def}
       />
 
-      <div className="pos-input" />
+      <TextField
+        className="pos-input"
+        inputProps={{ style: { fontSize: TextFieldFont.body1 } }}
+        margin="dense"
+        onChange={changePos}
+        placeholder="Choose part of speech"
+        select={true}
+        value={pos}
+      >
+        {selectablePoS.map(({ label, value }) => (
+          <MenuItem key={value} dense={true} value={value}>
+            {label}
+          </MenuItem>
+        ))}
+      </TextField>
     </div>
   );
 }
@@ -48,13 +77,30 @@ export default compose<Parameters<typeof VocabMeaningItem>[0], Props>(
           meaningIndex,
           vocabIndex
         })
-        .stringOrFail().value
+        .stringOrFail().value,
+      pos: getters
+        .getVocabMeaningItemProp(state, {
+          key: 'pos',
+          meaningIndex,
+          vocabIndex
+        })
+        .stringOrFail().value,
+      selectablePoS: allPoS
     }),
     (dispatch, { vocabIndex, meaningIndex }) => ({
       changeDef: ({ target: { value } }) =>
         dispatch(
           setters.setVocabMeaningItemProp({
             key: 'def',
+            meaningIndex,
+            value,
+            vocabIndex
+          })
+        ),
+      changePos: ({ target: { value } }) =>
+        dispatch(
+          setters.setVocabMeaningItemProp({
+            key: 'pos',
             meaningIndex,
             value,
             vocabIndex
