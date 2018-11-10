aebdb454990e30f9cc32c09c0aa7ef3c9e01134b
Hai Pham <swiften.svc@gmail.com>
2018-11-10 13:44:29 +0800

    feat(vocab-meaning): offload common meaning list logic to separate component

---
+++ src/components/vocab-meaning/list/component.tsx
@@ -0,0 +1,47 @@
+import { getters } from 'actions/vocab-meaning';
+import { onlyUpdateWhenDeepEqual } from 'components/utils';
+import { UndefinedProp } from 'javascriptutilities';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { compose, pure } from 'recompose';
+import { CombinedState } from 'reducers';
+import './style.scss';
+
+type Props = Readonly<{
+  vocabIndex: number;
+  ChildItem: React.ComponentType<
+    Readonly<{ vocabIndex: number; meaningIndex: number }>
+  >;
+}>;
+
+type StateProps = UndefinedProp<
+  Readonly<{ itemCount: number; itemIndexes: number[] }>
+>;
+
+function VocabMeaningList({
+  vocabIndex,
+  itemCount = 0,
+  itemIndexes = [],
+  ChildItem
+}: Props & StateProps) {
+  return (
+    <div className="vocab-meaning-container">
+      {[...itemIndexes, itemCount].map(meaningIndex => (
+        <ChildItem
+          key={meaningIndex}
+          vocabIndex={vocabIndex}
+          meaningIndex={meaningIndex}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default compose<Parameters<typeof VocabMeaningList>[0], Props>(
+  pure,
+  connect<StateProps, {}, Props, CombinedState>((state, { vocabIndex }) => ({
+    itemCount: getters.getAllVocabMeaningCount(state, vocabIndex).value,
+    itemIndexes: getters.getAllVocabMeaningIndexes(state, vocabIndex).value
+  })),
+  onlyUpdateWhenDeepEqual()
+)(VocabMeaningList);
--- src/components/vocab-pos/list/component.tsx
+++ src/components/vocab-pos/list/component.tsx
@@ -1,42 +1,13 @@
-import { getters } from 'actions/vocab-meaning';
-import { onlyUpdateWhenDeepEqual } from 'components/utils';
+import MeaningList from 'components/vocab-meaning/list/component';
 import Item from 'components/vocab-pos/item/component';
-import { UndefinedProp } from 'javascriptutilities';
+import { Omit } from 'javascriptutilities';
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { compose, pure } from 'recompose';
-import { CombinedState } from 'reducers';
-import './style.scss';
 
-type Props = Readonly<{ vocabIndex: number }>;
-
-type StateProps = UndefinedProp<
-  Readonly<{ itemCount: number; itemIndexes: number[] }>
+type Props = Omit<
+  Required<NonNullable<typeof MeaningList['defaultProps']>>,
+  'ChildItem'
 >;
 
-function VocabPosList({
-  vocabIndex,
-  itemCount = 0,
-  itemIndexes = []
-}: Props & StateProps) {
-  return (
-    <div className="vocab-pos-container">
-      {[...itemIndexes, itemCount].map(meaningIndex => (
-        <Item
-          key={meaningIndex}
-          vocabIndex={vocabIndex}
-          meaningIndex={meaningIndex}
-        />
-      ))}
-    </div>
-  );
+export default function VocabPosList({ vocabIndex }: Props) {
+  return <MeaningList vocabIndex={vocabIndex} ChildItem={Item} />;
 }
-
-export default compose<Parameters<typeof VocabPosList>[0], Props>(
-  pure,
-  connect<StateProps, {}, Props, CombinedState>((state, { vocabIndex }) => ({
-    itemCount: getters.getAllVocabMeaningCount(state, vocabIndex).value,
-    itemIndexes: getters.getAllVocabMeaningIndexes(state, vocabIndex).value
-  })),
-  onlyUpdateWhenDeepEqual()
-)(VocabPosList);
