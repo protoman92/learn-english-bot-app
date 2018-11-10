826601c36a9c464c69efc8742b457f20f5460098
Hai Pham <swiften.svc@gmail.com>
2018-11-11 00:55:53 +0800

    feat(sagas): replace direct api calls with call effects

--- src/sagas/vocabulary.ts
+++ src/sagas/vocabulary.ts
@@ -1,11 +1,11 @@
 import { ActionKey, getters, setters } from 'actions/vocabulary';
 import { Vocabulary } from 'data';
 import { Never, Try } from 'javascriptutilities';
-import { all, put, select, takeLatest } from 'redux-saga/effects';
+import { all, call, put, select, takeLatest } from 'redux-saga/effects';
 
 export default function(vocabApi: import('apis').Api['vocabulary']) {
   function* fetchVocabularies(api: typeof vocabApi) {
-    const vocabs = yield api.fetchVocabularies();
+    const vocabs = yield call(api.fetchVocabularies);
     yield put(setters.setVocabularies(vocabs));
   }
 
@@ -19,7 +19,7 @@ export default function(vocabApi: import('apis').Api['vocabulary']) {
       .filter(vocab => !!vocab)
       .map(vocab => vocab!);
 
-    const updatedVocabs = yield api.saveVocabularies(validVocabs);
+    const updatedVocabs = yield call(api.saveVocabularies, validVocabs);
     yield put(setters.setVocabularies(updatedVocabs));
   }
 
